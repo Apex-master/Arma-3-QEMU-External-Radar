49ce27194e206e8f0181da3a8937d9c4f56a8527
Nathan <84732301+Apex-master@users.noreply.github.com>
2022-08-22 01:41:48 +0100

    Update game.hpp

--- arma_dma/game.hpp
+++ arma_dma/game.hpp
@@ -190,6 +190,6 @@ namespace Data
 		const auto s = j.dump();
 		io.socket()->emit("json", s);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
